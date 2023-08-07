0474adc985b93b0cc5b8ddb00d511c7cd281278c
오세창 <114675677+sepang-pang@users.noreply.github.com>
2023-08-07 16:56:59 +0900

    init commit

---
+++ src/main/java/com/passion/teampassiontrelloproject/TeamPassionTrelloProjectApplication.java
@@ -0,0 +1,13 @@
+package com.passion.teampassiontrelloproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamPassionTrelloProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamPassionTrelloProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/passion/teampassiontrelloproject/TeamPassionTrelloProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passion.teampassiontrelloproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamPassionTrelloProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
