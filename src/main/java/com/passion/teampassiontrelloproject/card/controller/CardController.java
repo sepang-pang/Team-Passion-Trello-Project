ba5ffec55461d09768572cbe104e77edf0ad7b7f
GGombi <102966574+rhaql@users.noreply.github.com>
2023-08-08 10:40:51 +0900

    [featuer] [카드 CRUD] 1. 카드 CRUD 기능 구현  - 전체조회 구현 X , 선택조회만 가능

--- src/main/java/com/passion/teampassiontrelloproject/card/controller/CardController.java
+++ src/main/java/com/passion/teampassiontrelloproject/card/controller/CardController.java
@@ -1,4 +1,57 @@
 package com.passion.teampassiontrelloproject.card.controller;
 
+import com.passion.teampassiontrelloproject.card.dto.CardRequestDto;
+import com.passion.teampassiontrelloproject.card.dto.CardResponseDto;
+import com.passion.teampassiontrelloproject.card.service.CardService;
+import com.passion.teampassiontrelloproject.common.dto.ApiResponseDto;
+import com.passion.teampassiontrelloproject.common.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/card")
 public class CardController {
+    private final CardService cardService;
+
+    // 카드 선택조회
+    @GetMapping("/{id}")
+    public ResponseEntity<CardResponseDto> getCardById(@PathVariable Long id) {
+        CardResponseDto card = cardService.getCardById(id);
+        if (card != null) {
+            return new ResponseEntity<>(card, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // 카드 작성
+    @PostMapping
+    public ResponseEntity<CardResponseDto> createCard(@RequestBody CardRequestDto cardRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        CardResponseDto createCard = cardService.createPost(cardRequestDto, userDetails.getUser());
+        return new ResponseEntity<>(createCard, HttpStatus.CREATED);
+    }
+
+    // 카드 수정
+    @PutMapping("/{id}")
+    public ResponseEntity<CardResponseDto> updateCard(@PathVariable Long id, @RequestBody CardRequestDto cardRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        CardResponseDto updateCard = cardService.updateCard(id, cardRequestDto, userDetails.getUser());
+        if (updateCard != null) {
+            return new ResponseEntity<>(updateCard, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // 카드 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponseDto> deletePost(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        cardService.deleteCard(id, userDetails.getUser());
+        return ResponseEntity.ok().body(new ApiResponseDto("카드 삭제 성공", 200));
+    }
 }
+
+
--- src/main/java/com/passion/teampassiontrelloproject/card/entity/Card.java
+++ src/main/java/com/passion/teampassiontrelloproject/card/entity/Card.java
@@ -1,4 +1,50 @@
 package com.passion.teampassiontrelloproject.card.entity;
 
-public class Card {
+import com.passion.teampassiontrelloproject.card.dto.CardRequestDto;
+import com.passion.teampassiontrelloproject.common.entity.Timestamped;
+import com.passion.teampassiontrelloproject.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "card")
+@NoArgsConstructor
+public class Card extends Timestamped {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long card_id;
+
+    private String title;
+
+    private String username;
+
+    private String description;
+
+    private String background_color;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    public Card(CardRequestDto cardRequestDto, User user) {
+        this.title = cardRequestDto.getTitle();
+        this.description = cardRequestDto.getDescription();
+        this.background_color = cardRequestDto.getBackground_color();
+        this.username = user.getUsername();
+        this.user = user;
+
+    }
+
+    public void update(CardRequestDto cardRequestDto) {
+        this.title = cardRequestDto.getTitle();
+        this.description = cardRequestDto.getDescription();
+        this.background_color = cardRequestDto.getBackground_color();
+    }
 }
